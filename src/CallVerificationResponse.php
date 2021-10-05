6fa8bd2c8e2d2b680219b7f85c0301ca0e29e240
Igor <kotigo111@gmail.com>
2021-10-05 16:13:21 +0300

    change response data

--- src/CallVerificationResponse.php
+++ src/CallVerificationResponse.php
@@ -5,10 +5,19 @@ declare(strict_types=1);
 namespace Wearesho\Streamtele;
 
 use Horat1us\Yii\CarbonBehavior;
+use Horat1us\Yii\ConstRange;
 use yii\db;
 
 class CallVerificationResponse extends db\ActiveRecord
 {
+    public const STATE_NEW = 'new';
+    public const STATE_IN_PROCESSING = 'in_processing';
+    public const STATE_REJECTED = 'rejected';
+    public const STATE_NO_PICKUP = 'no_pickup';
+    public const STATE_DONE = 'done';
+    public const STATE_ERROR = 'error';
+    public const STATE_CONGESTION = 'congestion';
+
     public static function tableName(): string
     {
         return 'call_verification_response';
@@ -28,7 +37,8 @@ class CallVerificationResponse extends db\ActiveRecord
     public function rules(): array
     {
         return [
-            ['data', 'required'],
+            [['task_id', 'state', 'ivr_answer'], 'required'],
+            ['state', ConstRange\Validator::class],
         ];
     }
 
--- src/Migrations/M210410333333CreateCallVerificationResponseTable.php
+++ src/Migrations/M210410333333CreateCallVerificationResponseTable.php
@@ -7,6 +7,7 @@ use yii\db\Migration;
 class M210410333333CreateCallVerificationResponseTable extends Migration
 {
     private const TABLE_NAME = 'call_verification_response';
+    private const STATE_TYPE_NAME = self::TABLE_NAME . '_state_type';
 
     public function safeUp(): void
     {
@@ -14,7 +15,17 @@ class M210410333333CreateCallVerificationResponseTable extends Migration
             self::TABLE_NAME,
             [
                 'id' => $this->primaryKey(),
-                'data' => $this->json()->notNull(),
+                "task_id" => $this->text()->notNull(),
+                "state" => $this->enum(self::STATE_TYPE_NAME, [
+                    'new',
+                    'in_processing',
+                    'rejected',
+                    'no_pickup',
+                    'done',
+                    'error',
+                    'congestion',
+                ]),
+                "ivr_answer" => $this->integer()->notNull(),
                 'at' => $this->dateTime()->notNull(),
             ]
         );
@@ -23,5 +34,6 @@ class M210410333333CreateCallVerificationResponseTable extends Migration
     public function safeDown(): void
     {
         $this->dropTable(self::TABLE_NAME);
+        $this->execute("DROP TYPE " . self::STATE_TYPE_NAME);
     }
 }
